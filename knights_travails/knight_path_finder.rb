4ee4e86f27e069b6536ca9b5636bc1c8c756d859
evgenii-shvetsov <sea.dev91@gmail.com>
2022-10-24 10:57:40 -0700

    Finish phase_1

---
+++ knights_travails/knight_path_finder.rb
@@ -0,0 +1,16 @@
+require "../poly_tree_node/lib/00_tree_node.rb"
+
+class KnightPathFinder
+    def initialize(position)
+        @root_node = PolyTreeNode.new(position)
+    end
+
+    def build_move_tree
+
+    end
+
+    def find_path
+        
+    end
+end
+

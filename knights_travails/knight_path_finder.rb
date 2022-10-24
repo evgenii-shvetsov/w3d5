368da6ee192eb015581550eff44bce4793a697c0
evgenii-shvetsov <sea.dev91@gmail.com>
2022-10-24 11:26:58 -0700

    Complete Phase_1

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,8 +1,17 @@
 require "../poly_tree_node/lib/00_tree_node.rb"
 
 class KnightPathFinder
+    def self.valid_moves(pos)
+        
+    end
+
     def initialize(position)
         @root_node = PolyTreeNode.new(position)
+        @considered_positions = [position]
+    end
+
+    def new_move_positions(pos)
+        
     end
 
     def build_move_tree
